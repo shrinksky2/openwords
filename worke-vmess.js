6a90beb3eb00f1bad114ca00c37e2201ef91e056
shrinksky2 <102359792+shrinksky2@users.noreply.github.com>
2023-09-01 20:24:52 +0800

    Update worke-vmess.js

--- worke-vmess.js
+++ worke-vmess.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'd8940f50-d7b6-405e-b016-3e5052b4c884';
 
 let proxyIP = '';
 
